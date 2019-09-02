cb17a5b57ddb41451d978c7b51e42c036f171911
Petter <petterhoel>
2019-09-02 21:05:43 +0200

    adds some scaffolding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Dummy401Service } from './dummy401.service';
+import { HttpClient } from '@angular/common/http';
+import { delay } from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
@@ -10,7 +12,10 @@ export class AppComponent {
   title = 'httpchache';
   is401 = this.four.is401;
   isis401 = this.four.isis401;
-  constructor(public four: Dummy401Service) {}
+  employees: Employee[] = [];
+  constructor(
+    private http: HttpClient,
+    public four: Dummy401Service) { }
   toggle401(): void {
     this.four.toggle401();
   }
@@ -18,7 +23,26 @@ export class AppComponent {
     this.four.reset();
   }
 
+  getData(): void {
+    const url = 'http://dummy.restapiexample.com/api/v1/employees';
+    this.employees = [];
+    this.http.get<Employee[]>(url)
+      .pipe(delay(10))
+      .subscribe(employees => {
+        this.employees = employees;
+      });
+  }
+
   all401(): void {
     this.four.all401();
   }
+}
+
+
+interface Employee {
+    id: string;
+    employee_name: string;
+    employee_salary: string;
+    employee_age: string;
+    profile_image: string;
 }
